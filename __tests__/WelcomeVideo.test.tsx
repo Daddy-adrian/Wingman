b763d1138279ef0319cbecca4255b555580f06d1
adrian dawn <dawnadrian10@gmail.com>
2025-07-22 13:43:45 +0300

    feat: Phase 1.1 – WelcomeVideo component & tests

---
+++ __tests__/WelcomeVideo.test.tsx
@@ -0,0 +1,8 @@
+describe('WelcomeVideo', () => {
+  it('should render the video player on screen', () => {});
+  it('should automatically play the video when mounted', () => {});
+  it('should show a fallback message if video fails to load', () => {});
+  it('should allow skipping the video manually', () => {});
+  it('should automatically redirect after video finishes playing', () => {});
+  it('should not replay the video on re-mount if user already saw it', () => {});
+});
---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@expo|expo(nent)?|@testing-library)/)',
+  ],
+};
