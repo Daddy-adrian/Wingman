4a89db6a48c1648deb7cfea240a8063ef9c030d7
adrian dawn <dawnadrian10@gmail.com>
2025-07-25 11:45:44 +0300

    added prototype of skip button, needs to refactor

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,14 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 import WelcomeVideo from '@/components/WelcomeVideo';
 
-
+// using the WelcomeVideo file in the home screen
 export default function HomeScreen() {
   return (
    <WelcomeVideo />
   );
 }
-
+// styling the home screen
 const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
--- components/WelcomeVideo.tsx
+++ components/WelcomeVideo.tsx
@@ -1,22 +1,50 @@
-import { useEvent } from 'expo';
-import { useVideoPlayer, VideoView } from 'expo-video';
-import { StyleSheet, View, Button, Dimensions } from 'react-native';
 
+//imports
+import { useEvent } from 'expo';
+import { useRouter,useFocusEffect } from 'expo-router';
+import { useCallback } from 'react';
+import { useVideoPlayer, VideoView } from 'expo-video';
+import { Button, Dimensions, StyleSheet, View } from 'react-native';
+
+
+// link to video's url
 const videoSource =
   'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
 
+// function to display video
 export default function VideoScreen() {
   const player = useVideoPlayer(videoSource, player => {
     player.loop = true;
     player.play();
   });
 
+  // adding event to play or pause
   const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
+  
+  // initalizing router to handle skip button
+  const router = useRouter();
 
+  // stoping the video playback once routing to a new screen
+    useFocusEffect(
+    useCallback(() => {
+      return () => {
+        player.pause();
+      };
+    }, [player])
+  );
+
+  // video, controls and skip buttons views
   return (
     <View style={styles.contentContainer}>
       <VideoView style={styles.video} player={player} allowsFullscreen allowsPictureInPicture />
-      <View style={styles.controlsContainer}>
+      <View style={styles.skipButtonContainer}>
+          <Button
+           title="Skip"
+           onPress={() => router.navigate('/skip')}
+          />    
+      </View>
+       
+    <View style={styles.controlsContainer}>
         <Button
           title={isPlaying ? 'Pause' : 'Play'}
           onPress={() => {
@@ -32,6 +60,7 @@ export default function VideoScreen() {
   );
 }
 
+// setting dimension and style of video and buttons on screen
 const { width, height} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
@@ -47,9 +76,16 @@ const styles = StyleSheet.create({
     width: width,
     height: height,
   },
+    skipButtonContainer: {
+    position: 'absolute',      // layer it above the video
+    bottom: 100,               // place 100px up from bottom of screen
+    alignSelf: 'center',       // center horizontally
+    zIndex: 1,                 // ensure it sits above the play/pause View
+  },
   controlsContainer: {
     position: 'absolute',
     bottom: 50,
     alignSelf: 'center',
+    zIndex: 0,
   },
 });
